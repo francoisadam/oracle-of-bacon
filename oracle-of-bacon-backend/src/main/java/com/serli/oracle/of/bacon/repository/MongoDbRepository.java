ae4368a014c6649e89cc3144cde05630b49faebb
francois <francoisadam2@gmail.com>
2017-01-18 16:59:37 +0100

    get actor by name impemented with mongoDB

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/MongoDbRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/MongoDbRepository.java
@@ -8,6 +8,8 @@ import org.bson.Document;
 
 import java.util.Optional;
 
+import static com.mongodb.client.model.Filters.eq;
+
 public class MongoDbRepository {
 
     private final MongoClient mongoClient;
@@ -16,8 +18,35 @@ public class MongoDbRepository {
         mongoClient = new MongoClient("localhost", 27017);
     }
 
-    public Optional<Document> getActorByName(String name) {
+    public String getActorByName(String name) {
         // TODO implement actor fetch
-        return null;
+        String json = "";
+        try{
+            MongoDatabase db = mongoClient.getDatabase( "mongo" );
+            MongoCollection collection = db.getCollection("actors");
+            Document doc = (Document)collection.find(eq("name",name)).first();
+            return doc.toJson();
+        }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+
+        /*{
+            "_id":{
+            "$oid":"587bd993da2444c943a25161"
+        },
+            "imdb_id":"nm0000134",
+                "name":"Robert De Niro",
+                "birth_date":"1943-08-17",
+                "description":"Robert De Niro, thought of as one of the greatest actors of all time, was born in Greenwich Village, Manhattan, New York City, to artists Virginia (Admiral) and Robert De Niro Sr. His paternal grandfather was of Italian descent, and his other ancestry is Irish, German, Dutch, English, and French. He was trained at the Stella Adler Conservatory and...",
+                "image":"https://images-na.ssl-images-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_UY317_CR13,0,214,317_AL_.jpg",
+                "occupation":[
+            "actor",
+                    "producer",
+                    "soundtrack"
+   ]
+        }*/
+
+        return json;
+
     }
 }
